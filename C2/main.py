ed6eb032511c8f6eecbd5062625620f2eedea5fc
berkay-digital <46861579+berkay-digital@users.noreply.github.com>
2023-10-13 09:29:37 +0200

    Add class two

---
+++ C2/main.py
@@ -0,0 +1,39 @@
+list = ["asd", "asd", "hihiha"]
+# check variable list's type
+print(type(list))
+# check variable list's length
+print(len(list))
+# check variable list's first element
+print(list[0])
+# change string type to int type
+print(type(int("2")))
+# add two string
+print("2" + "2")
+# change binary type
+print(int("0b100", base=0))
+# check binary type
+print(type(0b100))
+# print some floats
+print(1.13)
+print(1.7e308)
+print(1.8e308)
+print(1e-324)
+print(5e-325)
+# use two line to print
+print("asd"
+      "asd")
+print("Hello \n World")
+# change float to int
+print(int(-2.23))
+# change float to string
+print(str(-2.2e3) + str(0b11))
+# check if 1 is equal to True
+print(1 == True)
+# check if 0 is equal to False
+print(0 == False)
+# use operators to calculate
+print(2 > 3 and 3 >= 2)
+print(2 != 3 or 3 == 4)
+print((2 + 3**2) / 5)
+print(1/4)
+print(4**2**3)
